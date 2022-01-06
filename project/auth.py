d8848c8d15e2351ec0928d6024a86aa79a56186a
Fox Imler <fox@imler.ca>
2022-01-05 19:49:28 -0700

    Update auth.py
    Fixed Login Exploit due to improper code execution

--- project/auth.py
+++ project/auth.py
@@ -21,7 +21,7 @@ def login_post():
     password = request.form.get('password')
     remember = True if request.form.get('remember') else False
     user = User.query.filter_by(email=email).first()
-    login_user(user,remember=remember)
+   
     # check if the user actually exists
     # take the user-supplied password, hash it, and compare it to the hashed password in the database
     if not user or not check_password_hash(user.password, password):
@@ -29,6 +29,7 @@ def login_post():
         return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
 
     # if the above check passes, then we know the user has the right credentials
+    login_user(user,remember=remember)
     return redirect(url_for('main.profile'))
 
 @auth.route('/logout')
