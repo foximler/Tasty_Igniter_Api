14960ae5414d28e35afee2cb5739a95fa4a91aa6
Fox Imler <fox@imler.ca>
2022-02-06 21:22:20 -0700

    Update api.py

--- project/api.py
+++ project/api.py
@@ -1,16 +1,24 @@
-printerAddress_expo = '192.168.0.21'
-printerAddress_kitchen = '192.168.0.20'
-printerPort = 9100
-from escposprinter import *
-from escposprinter.escpos import EscposIO, Escpos
+import base64
+import hashlib
+import random
+import re
 from datetime import datetime
+import mysql.connector
 import time
-api_key="MGRlYmNkMGVjYWY1MzE3NTkwNmZjMjBlYzE2ZDU0Mzg0MjBiZDUyZWU3MzA3NTJhYjIzNzE2ZDc5NDU0ZDVmMQ=="
-base_url="http://oldtrapper.gpeat.com:8080/api/"
+import json
+from phpserialize import *
+
+
+def generate_api_key():
+    """
+    @return: A hashkey for use to authenticate agains the API.
+    """
+    return base64.b64encode(hashlib.sha256(str(random.getrandbits(256)).encode('ascii')).hexdigest().encode("utf-8"))
+
 class orders: 
-    def __init__(self, order_id, order_status, order_printed, order_name,order_email,
-                 order_number,order_total_items,order_comments,
-                 pickup_time,order_total,order_items): 
+    def __init__(self, order_id, order_status, order_printed, order_name,order_email,order_number,
+                 order_total_items,order_comments,pickup_time,
+                 order_total,order_items): 
         self.order_id = order_id 
         self.order_status = order_status
         self.order_printed = order_printed
@@ -23,101 +31,74 @@ class orders:
         self.order_total = order_total
         self.order_items = order_items
 
-import requests
-import json
+
+def generate_expoview(order_in):
+    items_list = []
+    item_object =  loads(order_in[8].encode(), object_hook=phpobject)
+    order_name = f"{order_in[2]} {order_in[3]}"
+    order_price = round(float(order_in[17]),2)
+    d = datetime.strptime(str(order_in[15]), "%H:%M:%S")
+    item_list = []
+    for items in item_object._asdict()[b'\x00*\x00items'].items():
+        item={"Item_name":"","Item_price":"","Item_mods":[],"Item_comments":""}
+        item["Item_name"] = items[1]._asdict()[b'name'].decode('utf-8')
+        item["Item_price"] = items[1]._asdict()[b'price']
+        item_mods = items[1]._asdict()[b'options']._asdict()[b'\x00*\x00items'].items()
+        mods = []
+        if bool(item_mods):
+            item["Item_comments"] = items[1]._asdict()[b'comment'].decode('utf-8')
+            for mod in item_mods:
+                mods_list = mod[1]._asdict()[b'values']._asdict()[b'\x00*\x00items'].items()
+                for z in mods_list:
+                    i = 1
+                    while i <= z[1]._asdict()[b'qty']:
+                        mods.append(z[1]._asdict()[b'name'].decode('utf-8'))
+                        i += 1
+            item["Item_mods"] = mods
+        i=1
+        while i <= int(items[1]._asdict()[b'qty']): 
+            items_list.append(item)
+            i += 1
+    Holla_back = orders(order_in[0],order_in[18],order_in[30],order_name,order_in[4],order_in[5],order_in[9],order_in[10],d.strftime('%I:%M %p'),order_price,items_list)
+    return Holla_back
 
 def get_active_orders():
-    url = f'{base_url}active_orders'
-    myobj = {'api_key': api_key}
+    mydb = mysql.connector.connect(
+          host="",
+          user="",
+          password="",
+          database=""
+    )
 
-    data = requests.post(url, data = myobj)
-    items = []
-    for y in data.json():
-        x = json.loads(y)
-        items.append(orders(x["order_id"],x["order_status"],x["order_printed"],x["order_email"],x["order_name"],x["order_number"],x["order_total_items"],x["order_comments"],x["order_pickup_time"],x["order_total"],x["order_items"]))
-    return items
-def checkPrinterAlive(printerAddress,printerPort):
-        if (printer.Network.isAlive(printerAddress, printerPort)):
-            return True
-        else:
-            return True
-            raise Exception ("Host is unreachable, socket communication was not opened")
-def generate_kitchenview(order_in):
-    if (checkPrinterAlive(printerAddress_kitchen,printerPort)):
-        with EscposIO(printer.Network(printerAddress_kitchen, printerPort)) as p:
-            p.set(font='b', codepage='cp1251', size='2x', align='center', bold=False,color=2)
-            p.writelines('')
-            p.writelines(f"Order Id: {order_in.order_id}")
-            p.writelines(f"Pickup Time: {order_in.order_pickup_time}")
-            p.writelines(f"Order Comments: {order_in.order_comments}")
-            p.writelines('')
-            for x in order_in.order_items:
-                z = 0
-                while z < int(x["Item_quantity"]):
-                    z = z+1
-                    p.set(font='b', codepage='cp1251', size='2x', align='left', bold=False,color=2)
-                    p.writelines(x["Item_name"])
-                    for mod in x["Item_mods"]:
-                        p.set(font='a', codepage='cp1251', size='2h', align='left', bold=False,color=2)
-                        p.writelines(f'   Mods: {mod}')
-                    p.set(font='a', codepage='cp1251', size='2h', align='right', bold=True,color=1)
-                    p.writelines(x["Item_comments"])
-                    p.writelines(' ')
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT * FROM ti_orders WHERE status_id != 5")
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    myresult = mycursor.fetchall()
+    lists = []
+    for x in myresult:
+        lists.append(json.dumps(generate_expoview(x).__dict__))
+    return lists
 
-def generate_expoview(order_in):                
-    if (checkPrinterAlive(printerAddress_expo,printerPort)):
-        with EscposIO(printer.Network(printerAddress_expo, printerPort)) as p:
-            p.set(font='b', codepage='cp1251', size='normal', align='center', bold=False,color=2)
-            p.writelines(f"Order Id: {order_in.order_id}")
-            p.writelines('')
-            p.writelines(f"Order Name:  {order_in.order_name}")
-            p.writelines('')
-            p.writelines(f"Phone Number:  {order_in.order_number}")
-            p.writelines('')
-            p.writelines(f"Total Items: {order_in.order_total_items}")
-            p.writelines('')
-            p.writelines(f"Order Comments: {order_in.order_comments}")
-            p.writelines('')
-            p.writelines('')
-            p.writelines(f"Pickup Time: {order_in.order_pickup_time}")
-            p.writelines('')
-            p.writelines(f"Order Total: ${order_in.order_total}")
-            p.writelines('')
-            for x in order_in.order_items:
-                z = 0
-                
-                while z < int(x["Item_quantity"]):
-                    z = z+1
-                    p.set(font='b', codepage='cp1251', size='2x', align='left', bold=False,color=2)
-                    p.writelines(x["Item_name"])
-                    for mod in x["Item_mods"]:
-                        p.set(font='a', codepage='cp1251', size='2h', align='left', bold=False,color=2)
-                        p.writelines(f'   Mods: {mod}')
-                    p.set(font='a', codepage='cp1251', size='2h', align='right', bold=True,color=1)
-                    p.writelines(x["Item_comments"])
-                    p.writelines(' ')
-def update_order_status(order_id, order_status):
-    url = f'{base_url}update_order_status'
-    myobj = {'order_id': order_id,'order_status':order_status,'api_key':api_key}
-    x = requests.post(url, data = myobj)
+def update_order_status(order_id,order_status):
+    mydb = mysql.connector.connect(
+          host="",
+          user="",
+          password="",
+          database=""
+    )
+    mycursor = mydb.cursor()
+    sql= f"UPDATE ti_orders SET status_id = {int(order_status)} WHERE order_id = {int(order_id)}"
+    mycursor.execute(sql)
+    mydb.commit()
 def update_print_status(order_id, print_status):
-    url = f'{base_url}update_print_status'
-    myobj = {'order_id': order_id,'print_status':print_status,'api_key':api_key}
-    x = requests.post(url, data = myobj)
-while True:
-    try:
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        print("Orders Last Checked at: ", current_time, end="\r")
-        myresult = get_active_orders()
-        for x in myresult:
-           if x.order_printed == 0:
-               generate_expoview(x)
-               generate_kitchenview(x)
-               update_print_status(x.order_id,1)
-               update_order_status(x.order_id,3)
-
-        time.sleep(15)
-    except:
-       print("fail")
-       time.sleep(30)
+    mydb = mysql.connector.connect(
+          host="",
+          user="",
+          password="",
+          database=""
+    )
+    mycursor = mydb.cursor()    
+    sql= f"UPDATE ti_orders SET order_printed = {int(print_status)} WHERE order_id = {int(order_id)}"
+    mycursor.execute(sql)
+    mydb.commit()
