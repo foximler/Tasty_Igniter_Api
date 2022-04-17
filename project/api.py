52bbed852a81c96925b5ced1016837da3ce30531
Fox Imler <fox@imler.ca>
2022-04-17 11:21:22 -0600

    Update api.py
    Updated to support delivery

--- project/api.py
+++ project/api.py
@@ -18,7 +18,7 @@ def generate_api_key():
 class orders: 
     def __init__(self, order_id, order_status, order_printed, order_name,order_email,order_number,
                  order_total_items,order_comments,pickup_time,
-                 order_total,order_items): 
+                 order_total,order_items, order_type, order_city, order_address, order_state): 
         self.order_id = order_id 
         self.order_status = order_status
         self.order_printed = order_printed
@@ -30,10 +30,23 @@ class orders:
         self.order_pickup_time = pickup_time
         self.order_total = order_total
         self.order_items = order_items
-
+        self.order_type = order_type
+        self.order_address = order_address
+        self.order_city = order_city
+        self.order_state = order_state
 
 def generate_expoview(order_in):
     items_list = []
+    order_type = order_in[13]
+    if (order_type == "delivery" ):
+        info  = get_order_address(order_in[7])
+        order_city = info.city
+        order_address = info.address
+        order_state = info.state
+    else:
+        order_city = ""
+        order_address = ""
+        order_state = ""
     item_object =  loads(order_in[8].encode(), object_hook=phpobject)
     order_name = f"{order_in[2]} {order_in[3]}"
     order_price = round(float(order_in[17]),2)
@@ -59,7 +72,7 @@ def generate_expoview(order_in):
         while i <= int(items[1]._asdict()[b'qty']): 
             items_list.append(item)
             i += 1
-    Holla_back = orders(order_in[0],order_in[18],order_in[30],order_name,order_in[4],order_in[5],order_in[9],order_in[10],d.strftime('%I:%M %p'),order_price,items_list)
+    Holla_back = orders(order_in[0],order_in[18],order_in[30],order_name,order_in[4],order_in[5],order_in[9],order_in[10],d.strftime('%I:%M %p'),order_price,items_list,order_type,order_city,order_address,order_state)
     return Holla_back
 
 def get_active_orders():
@@ -79,6 +92,25 @@ def get_active_orders():
     for x in myresult:
         lists.append(json.dumps(generate_expoview(x).__dict__))
     return lists
+
+def get_order_address(address):
+    mydb = mysql.connector.connect(
+          host="",
+          user="",
+          password="",
+          database=""
+    )
+
+    mycursor = mydb.cursor()
+    mycursor.execute(f"SELECT * FROM ti_addresses WHERE address_id = {int(address)}")
+    myresult = mycursor.fetchall()
+    location = {
+    address: myresult[2]+' - '+myresult[3],
+    city: myresult[4],
+    state: myresult[5]
+    }
+    return location
+
 
 def update_order_status(order_id,order_status):
     mydb = mysql.connector.connect(
